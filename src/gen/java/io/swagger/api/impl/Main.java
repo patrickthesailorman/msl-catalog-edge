050441fe5c720f2beb4e358434348e9911bf183e
anram88 <aramirez@kenzanmedia.com>
2016-02-05 16:00:53 -0600

    MSL-394: Update Architecture to account for Karyon

---
+++ src/gen/java/io/swagger/api/impl/Main.java
@@ -0,0 +1,61 @@
+package io.swagger.api.impl;
+
+import io.swagger.api.CatalogEdgeApi;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.servlet.ServletContainer;
+
+import com.netflix.governator.annotations.Modules;
+import netflix.karyon.KaryonBootstrap;
+import netflix.karyon.archaius.ArchaiusBootstrap;
+import netflix.karyon.ShutdownModule;
+import netflix.karyon.servo.KaryonServoModule;
+
+import javax.servlet.DispatcherType;
+import java.util.EnumSet;
+import netflix.adminresources.resources.KaryonWebAdminModule;
+import netflix.karyon.jersey.blocking.KaryonJerseyModule;
+
+@ArchaiusBootstrap
+@KaryonBootstrap(name = "msl-catalog-edge")
+@Modules(include = {
+        ShutdownModule.class,
+        KaryonWebAdminModule.class, // Uncomment this to enable WebAdmin
+        //KaryonEurekaModule.class, // Uncomment this to enable Eureka client.
+        KaryonServoModule.class
+})
+public class Main {
+
+    /**
+     * Runs jetty server to expose jersey API
+     * @param args String array
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+
+        Server jettyServer = new Server(9003);
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        context.addFilter(CatalogEdgeApiOriginFilter.class,  "/*",
+                EnumSet.of(DispatcherType.REQUEST));
+        jettyServer.setHandler(context);
+
+        ServletHolder jerseyServlet = context.addServlet(ServletContainer.class, "/*");
+        jerseyServlet.setInitOrder(0);
+
+        jerseyServlet.setInitParameter(
+                "jersey.config.server.provider.classnames",
+                CatalogEdgeApi.class.getCanonicalName()
+        );
+
+        try {
+
+            jettyServer.start();
+            jettyServer.join();
+
+        } finally {
+            jettyServer.destroy();
+        }
+    }
+}
